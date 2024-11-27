8313696196142b270207d5395fe25979417f20f9
Kenan Ben <gazokenan@gmail.com>
2024-11-27 21:11:51 +0800

    add protag movement

--- spacebar/main_protag.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$ProtagAnimation.play("walk_right")
-	$ProtagAnimation.play("walk_up")
---
+++ spacebar/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var speed := 500
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(100, 500)
+	
+	
+
+func _process(delta: float):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	position += direction * speed * delta
+		
+	
