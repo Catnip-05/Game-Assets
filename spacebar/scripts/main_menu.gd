e6b4a2a8613c4d3a14b590e64ae77cac8b2563ec
Kenan Ben <gazokenan@gmail.com>
2024-11-28 23:28:46 +0800

    connect-stg1

--- spacebar/scripts/main_menu.gd
+++ spacebar/scripts/main_menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 @onready var start_button = $"TextureRect/MarginContainer/HBoxContainer/VBoxContainer/Start Button" as Button
 @onready var exit_button = $"TextureRect/MarginContainer/HBoxContainer/VBoxContainer/Exit Button" as Button
-# @export start_level = preload(main.tscn)
+@onready var start_level = preload("res://scenes/stg1.tscn")
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
@@ -12,8 +12,7 @@ func _ready():
 	
 	
 func on_start_pressed() -> void:
-	#get_tree().change_scene_to_packed(start_level)
-	pass
+	get_tree().change_scene_to_packed(start_level)
 	
 func on_exit_pressed() -> void:
 	get_tree().quit()
