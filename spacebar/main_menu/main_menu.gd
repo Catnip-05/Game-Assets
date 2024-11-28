74c7bf8305f7c335d8880a011c3617f7bfe56807
Kenan Ben <gazokenan@gmail.com>
2024-11-28 08:35:00 +0800

    initial-script-main-menu

---
+++ spacebar/main_menu/main_menu.gd
@@ -0,0 +1,18 @@
+class_name MainMenu
+extends Control
+
+
+@onready var start_button = $"TextureRect/MarginContainer/HBoxContainer/VBoxContainer/Start Button" as Button
+@onready var exit_button = $"TextureRect/MarginContainer/HBoxContainer/VBoxContainer/Exit Button" as Button
+# @export start_level = preload(main.tscn)
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	
+	
+func on_start_pressed() -> void:
+	pass
+	
+func on_exit_pressed() -> void:
+	get_tree().quit()
