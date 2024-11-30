20c69311d25c7c256d9ee7578afaac96f299dd1b
Kenan Ben <gazokenan@gmail.com>
2024-11-30 22:30:23 +0800

    change-dragging-logic

--- spacebar/scripts/puzzle.gd
+++ spacebar/scripts/puzzle.gd
@@ -1,27 +1,52 @@
 extends Node2D
 
-var selected = false
-var offset = Vector2()
-var drop_area : Area2D
-var snap_position : Vector2 = Vector2(500, 500)
+var draggable = false
+var is_inside_droppable = false
+var body_ref
+var initialPos: Vector2  
+var offset: Vector2  
+var is_dragging = false
 
 func _ready() -> void:
 	pass
-	
+
 func _process(delta: float) -> void:
-	if selected: 
-		followMouse()
-		
-func followMouse():
-	position = get_global_mouse_position() + offset
-	
+	if draggable:
+		if Input.is_action_just_pressed('click'):
+			initialPos = global_position
+			offset = get_global_mouse_position() - global_position
+			is_dragging = true
+		if Input.is_action_just_pressed('click'):
+			global_position = get_global_mouse_position() - offset
+		elif Input.is_action_just_released('click'):
+			is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_droppable:
+				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(tween.EASE_OUT)
+			else:
+				tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(tween.EASE_OUT)
 
 
+func _on_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group('droppable'):
+		is_inside_droppable = true
+		body.modulate= Color(Color.REBECCA_PURPLE, 1)
+		body_ref
 
-func _on_cross_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			selected = true
-			offset = position - get_global_mouse_position()
-		else:
-			selected = false
+
+func _on_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group('droppable'):
+		is_inside_droppable = false
+		body.modulate= Color(Color.MEDIUM_PURPLE, 0.7)
+
+
+func _on_area_mouse_entered() -> void:
+	if not is_dragging:
+		draggable = true
+		scale = Vector2(1.05, 1.05)
+
+
+func _on_area_mouse_exited() -> void:
+	if not is_dragging:
+		draggable = false
+		scale = Vector2(1,1)
