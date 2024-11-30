5d25f68c52a56c053fb7f3137753d195682b6546
Kenan Ben <gazokenan@gmail.com>
2024-11-30 18:50:56 +0800

    adjust size

--- spacebar/scripts/clickable.gd
+++ spacebar/scripts/clickable.gd
@@ -12,3 +12,5 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			show_popup()
+			
+# timer optional
