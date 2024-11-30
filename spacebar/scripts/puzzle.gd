53cf3c4ebb589a8c4584d0df428e3407eba6945e
Kenan Ben <gazokenan@gmail.com>
2024-11-30 23:22:04 +0800

    engine-repair-puzzle

--- spacebar/scripts/puzzle.gd
+++ spacebar/scripts/puzzle.gd
@@ -15,11 +15,11 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed('click'):
 			initialPos = global_position
 			offset = get_global_mouse_position() - global_position
-			is_dragging = true
+			is_dragging = true #.global
 		if Input.is_action_just_pressed('click'):
 			global_position = get_global_mouse_position() - offset
 		elif Input.is_action_just_released('click'):
-			is_dragging = false
+			is_dragging = false  #.global
 			var tween = get_tree().create_tween()
 			if is_inside_droppable:
 				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(tween.EASE_OUT)
@@ -41,12 +41,12 @@ func _on_area_body_exited(body: Node2D) -> void:
 
 
 func _on_area_mouse_entered() -> void:
-	if not is_dragging:
+	if not is_dragging:  #.global
 		draggable = true
 		scale = Vector2(1.05, 1.05)
 
 
 func _on_area_mouse_exited() -> void:
-	if not is_dragging:
+	if not is_dragging:  #.global
 		draggable = false
 		scale = Vector2(1,1)
