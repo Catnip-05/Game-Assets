586ffaf7991cabd7d3362da292e604d6b316510d
Kenan Ben <gazokenan@gmail.com>
2024-11-30 21:01:04 +0800

    draggable object

--- spacebar/scripts/puzzle.gd
+++ spacebar/scripts/puzzle.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
-var puzzle_pieces = []  
-var correct_positions = []  
-var puzzle_solved = false
+var selected = false
 
 func _ready() -> void:
-	puzzle_pieces = [get_node("PuzzleArea/Sprite2D"), get_node("PuzzleArea/Sprite2D2")] 
-	correct_positions = [Vector2(100, 200), Vector2(200, 300)] 
-	for piece in puzzle_pieces:
-		piece.connect("input_event", self, "_on_piece_input_event")
-
-
-
-func _process(delta: float) -> void:
 	pass
+	
+func _process(delta: float) -> void:
+	if selected: 
+		followMouse()
+		
+func followMouse():
+	position = get_global_mouse_position()
+	
+
 
 
 func _on_cross_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			selected = true
+		else:
+			selected = false
