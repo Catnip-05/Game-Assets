2426500ca60a251c86f131b477ea8619a35fb10f
Kenan Ben <gazokenan@gmail.com>
2024-11-30 18:45:01 +0800

    add-label

--- spacebar/scripts/clickable.gd
+++ spacebar/scripts/clickable.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 
+
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		# Check if the left mouse button was clicked
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			show_popup()
 
-# Function to show the popup
 func show_popup():
 	print("Object clicked! Show something here.")
+	if $Label:
+		$Label.visible = true
