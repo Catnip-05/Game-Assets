045f23c5fa8e93f942fbb9d25ebf67ea6fb5658c
Kenan Ben <gazokenan@gmail.com>
2024-11-30 18:47:51 +0800

    fix-input-signal

--- spacebar/scripts/clickable.gd
+++ spacebar/scripts/clickable.gd
@@ -2,12 +2,13 @@ extends Area2D
 
 
 
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			show_popup()
-
 func show_popup():
 	print("Object clicked! Show something here.")
 	if $Label:
 		$Label.visible = true
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			show_popup()
